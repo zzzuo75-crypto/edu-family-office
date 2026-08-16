3266b96072d25793262c55f11fdda08e21890384
zzzuo75-crypto <zzzuo75@gmail.com>
2026-08-16 20:29:11 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-<main className="flex-grow">
-  {legalPage ? (
-    <LegalPages
-      page={legalPage}
-      onBack={() => setLegalPage(null)}
-    />
-  ) : (
-    <>
-      {/* 保留你原本的網站頁面內容 */}
-    </>
-  )}
-</main>
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
