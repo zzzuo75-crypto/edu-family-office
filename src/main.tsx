dbd9b767e9c28903fd89dd27c23a7dddda26dcdb
zzzuo75-crypto <zzzuo75@gmail.com>
2026-08-16 20:28:00 +0800

    Add conditional rendering for legal pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,18 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+<main className="flex-grow">
+  {legalPage ? (
+    <LegalPages
+      page={legalPage}
+      onBack={() => setLegalPage(null)}
+    />
+  ) : (
+    <>
+      {/* 保留你原本的網站頁面內容 */}
+    </>
+  )}
+</main>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
